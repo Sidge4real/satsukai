815c5b5ca91642275e0c2490a48ad360a2a10fcc
Lukas <vanderspiegel.lukas@gmail.com>
2023-12-01 11:17:43 +0000

    navlink added

--- satsukai.frontend/src/components/header.tsx
+++ satsukai.frontend/src/components/header.tsx
@@ -18,7 +18,7 @@ const HeaderComponent = () => {
           <div className="flex items-center space-x-4">
             <Link href="/" className={`text-gray-500 hover:bg-gray-100 rounded-md px-3 py-2 text-sm font-medium ${router.pathname === '/' ? 'bg-gray-100' : ''}`}>Home</Link>
             <Link href="/products" className={`text-gray-500 hover:bg-gray-100 rounded-md px-3 py-2 text-sm font-medium ${router.pathname === '/shop' ? 'bg-gray-100' : ''}`}>Shop</Link>
-            <a href="#" className={`text-gray-500 hover:bg-gray-100 rounded-md px-3 py-2 text-sm font-medium ${router.pathname === '/blog' ? 'bg-gray-100' : ''}`}>Blog</a>
+            <a href="/posts" className={`text-gray-500 hover:bg-gray-100 rounded-md px-3 py-2 text-sm font-medium ${router.pathname === '/blog' ? 'bg-gray-100' : ''}`}>Blog</a>
             <a href="#" className={`text-gray-500 hover:bg-gray-100 rounded-md px-3 py-2 text-sm font-medium ${router.pathname === '/contact' ? 'bg-gray-100' : ''}`}>Contact</a>
             <button type="button" className="text-gray-500 hover:bg-gray-100 rounded-md px-3 py-2">
               <span className="material-symbols-outlined">shopping_bag</span>
