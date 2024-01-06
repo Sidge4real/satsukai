e444c09240b8e3a4b6ff45d7b9474749d9132926
Lukas <vanderspiegel.lukas@gmail.com>
2024-01-06 15:05:44 +0000

    change document title

--- satsukai.frontend/src/pages/_document.tsx
+++ satsukai.frontend/src/pages/_document.tsx
@@ -3,12 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <body>
       <Head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
         <link href="./styles/global.css" rel="stylesheet" />
         <link rel="icon" href="favicon.png" />
+        <title>Satsukai</title>
       </Head>
-      <body>
         <Main />
         <NextScript />
       </body>
